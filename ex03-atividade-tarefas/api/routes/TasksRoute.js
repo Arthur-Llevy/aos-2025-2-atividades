a7dfa5f058ef4c08f904fdf541ec3bf376c8ea23
Arthur Levy <arthurlbvieiraa@gmail.com>
2025-09-27 20:18:23 -0300

    feat: atividade concluída

--- ex03-atividade-tarefas/api/routes/TasksRoute.js
+++ ex03-atividade-tarefas/api/routes/TasksRoute.js
@@ -3,10 +3,10 @@ import { getAllTasks, createTask, getTaskById, deleteTask, updateTask } from "..
 
 const router = Router();
 
-router.get("tarefas", getAllTasks);
-router.post("tarefas", createTask);
-router.get("tarefas/:id", getTaskById);
-router.delete("tarefas/:id", deleteTask);
-router.put("tarefas/:id", updateTask);
+router.get("/tarefas", getAllTasks);
+router.post("/tarefas", createTask);
+router.get("/tarefas/:id", getTaskById);
+router.delete("/tarefas/:id", deleteTask);
+router.put("/tarefas/:id", updateTask);
 
 export default router;
